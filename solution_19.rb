566a2e0b039c96cef265d5f5264e5fa575c8a80e
Michael Scriven <bigscriv03@gmail.com>
2014-10-02 22:09:49 -0400

    Added symbol to day of the week, #2

--- solution_19.rb
+++ solution_19.rb
@@ -1,32 +1,12 @@
 
-require 'Date'
+require 'date'
 
 require 'active_support'
 require 'active_support/all'
-=begin
-Create a new repository
-Write a Ruby script that uses the Rails ActiveSupport gem, installed via a Gemfile and bundler
-The script should do the following when run:
-
-Show the date of the beginning of the current week
-Show what the date will be in 30 days
-Show what day of the week today's date will fall on next year
-Show what day of the week the user's birthday will be on this year (don't forget to ask for it!)
-Using the String class, pluralize a singular word the user enters and show the singular for a plural word the user enters
-Using the String class, truncate a phrase the user enters to 20 letters
-Using the Array class, convert an array of three words to a sentence with one command
 
 
-
-=end
-class << self
-    attr_accessor :beginning_of_week_default
-
-    def beginning_of_week
-      Thread.current[:beginning_of_week] || beginning_of_week_default || :Monday
-    end
-end
-      puts beginning_of_week
+Date.beginning_of_week = :monday
+puts Date.beginning_of_week
 
 
     def date_in_30_days
